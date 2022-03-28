5b1e05896821f3904f9886ce0c8221d02b909e31
Lucas Michailian <lucasmichailian@gmail.com>
2022-03-28 14:25:02 -0300

    add some tests

--- src/tests/integration_test.rs
+++ src/tests/integration_test.rs
@@ -1,11 +1,14 @@
 use assert_cmd::prelude::*;
 use std::process::Command; 
+use predicates::prelude::*; 
 
 #[test]
 fn run_with_defaults() -> Result<(), Box<dyn std::error::Error>> {
     Command::current_dir("catsay")
         .expect("binary exists")
         .assert()
-        .success();
+        .success()
+        .stdout(predicate::str::contains("Meow!"));
+        
     Ok(())
 }
